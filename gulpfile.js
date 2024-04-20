cc0e1f5372ded4a3b6a671eca84343f513072760
biapsilva <127308314+biapsilva@users.noreply.github.com>
2024-04-20 20:24:00 -0300

    Hero

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
     return gulp.src('./src/Styles/*.scss')
@@ -7,7 +8,13 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles
+function images() {
+    return gulp.src('./src/Images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/Images'));
+}
+
+exports.default = gulp.parallel(styles, images);
 exports.watch = function() {
     gulp.watch('./src/Styles/*.scss', gulp.parallel(styles))
 }
